78254a49d46d574d542b309a000adf6fbf9d1bbd
Paul Fawkesley <paul@fluidkeys.com>
2018-12-05 12:24:48 +0000

    add deps from fluidkeys/{crypto, fluidkeys/pgpkey}

---
+++ vendor/github.com/fluidkeys/fluidkeys/openpgpdefs/compression/compression.go
@@ -0,0 +1,38 @@
+package compression
+
+import (
+	"fmt"
+)
+
+type CompressionAlgorithm = uint8
+
+const (
+	// https://tools.ietf.org/html/rfc4880#section-9.3
+	Uncompressed CompressionAlgorithm = 0
+	ZIP                               = 1
+	ZLIB                              = 2
+	BZIP2                             = 3
+)
+
+func Name(compressionAlgorithm CompressionAlgorithm) string {
+	switch compressionAlgorithm {
+	case Uncompressed:
+		return "Uncompressed"
+
+	case ZIP:
+		return "ZIP"
+
+	case ZLIB:
+		return "ZLIB"
+
+	case BZIP2:
+		return "BZIP2"
+
+	case 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110:
+		return fmt.Sprintf("Private/experimental (%d)", compressionAlgorithm)
+
+	default:
+		return fmt.Sprintf("Unknown (%d)", compressionAlgorithm)
+	}
+
+}
