0923c8403cf059ad760c35485bc28fa7c84fa403
Paul Fawkesley <paul@fluidkeys.com>
2018-12-10 16:26:35 +0000

    verify(..) function for clearsigned data

--- server/encrypt.go
+++ server/encrypt.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/fluidkeys/crypto/openpgp"
 	"github.com/fluidkeys/crypto/openpgp/armor"
+	"github.com/fluidkeys/crypto/openpgp/clearsign"
 
 	"github.com/fluidkeys/fluidkeys/pgpkey"
 )
@@ -31,4 +33,22 @@ func encryptStringToArmor(secret string, pgpKey *pgpkey.PgpKey) (string, error) 
 	pgpWriteCloser.Close()
 	message.Close()
 	return buffer.String(), nil
+}
+
+func verify(clearsignedData []byte, publicKey *pgpkey.PgpKey) (verifiedPlaintext []byte, err error) {
+	block, _ := clearsign.Decode([]byte(clearsignedData))
+	if block == nil {
+		return nil, fmt.Errorf("error finding clearsigned data")
+	}
+
+	var keyring openpgp.EntityList = []*openpgp.Entity{&publicKey.Entity}
+
+	signer, err := openpgp.CheckDetachedSignature(keyring, bytes.NewBuffer(block.Bytes), block.ArmoredSignature.Body)
+	if err != nil {
+		return nil, fmt.Errorf("signature error: %v", err)
+	} else if signer.PrimaryKey.Fingerprint != publicKey.Entity.PrimaryKey.Fingerprint {
+		return nil, fmt.Errorf("signed by wrong key")
+	}
+
+	return block.Plaintext, nil
 }
